1a29c6f174ac5cedfa4c95aaac1fb18decf7e5e2
Izaaq <odongoizaaq@gmail.com>
2024-07-17 09:54:38 -0400

    commit

--- server/app.py
+++ server/app.py
@@ -1,27 +1,47 @@
-# server/app.py
-
 from flask import Flask, jsonify, make_response
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
+from flask_bcrypt import Bcrypt
 
 from models import db, User, Review, Game
 
 app = Flask(__name__)
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///app.db"
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
-app.json.compact = False
-
-migrate = Migrate(app, db)
 
 db.init_app(app)
+migrate = Migrate(app, db)
+bcrypt = Bcrypt(app)
 
 @app.route('/')
 def index():
     return "Index for Game/Review/User API"
 
-# start building your API here
+@app.route('/games')
+def games():
+    games = [game.to_dict() for game in Game.query.all()]
 
+    return jsonify(games), 200
+
+@app.route('/games/<int:id>')
+def game_by_id(id):
+    game = Game.query.get(id)
+
+    if not game:
+        return make_response(jsonify({"error": "Game not found"}), 404)
+
+    return jsonify(game.to_dict()), 200
+
+@app.route('/games/users/<int:id>')
+def game_users_by_id(id):
+    game = Game.query.get(id)
+
+    if not game:
+        return make_response(jsonify({"error": "Game not found"}), 404)
+
+    users = [user.to_dict(rules=("-reviews",)) for user in game.users]
+
+    return jsonify(users), 200
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
-
